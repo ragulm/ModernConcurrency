4deb6c75cc83e43aa266a18ad41358d3e1c08ddd
ML <ragul.lakshmanan@gmail.com>
2025-04-16 19:11:06 +0530

    Changing content view with custom views

--- MLModernConcurrency/MLModernConcurrencyApp.swift
+++ MLModernConcurrency/MLModernConcurrencyApp.swift
@@ -15,17 +15,19 @@ struct MLModernConcurrencyApp: App {
             Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
-
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //Utilsize as required 
+            AsyncAwaitView()
+            //            PublisherView()
         }
         .modelContainer(sharedModelContainer)
     }
